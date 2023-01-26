b751c17187617a24aaa0fec13409a197ecdbce19
Ricardo <ricardosala@grupotesys.es>
2023-01-27 00:43:03 +0100

    uploads picture from url and fetchs data for face recognition

--- src/App.js
+++ src/App.js
@@ -4,23 +4,101 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
+import { Component } from 'react';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication, app ID, model details, and the URL
+    // of the image we want as an input. Change these strings to run your own example.
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+    const USER_ID = 'ricsala';
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '0c16d5dd9ffc480c9f61507bda649cb3';
+    const APP_ID = 'face-recognition';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+  ///////////////////////////////////////////////////////////////////////////////////
+
+
+
+  
 const config = {
   'num' : 400
 }
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+        // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+
+    this.setState({ imageUrl: this.state.input });
+
+    const IMAGE_URL = this.state.input
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+    });
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+  }
+
+  render() {
+    return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      <ParticlesBg type="tadpole" bg={true} config={config} color="#000000"/>
+      <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit} />
+      <ParticlesBg type="tadpole" bg={true} config={config} color="#000000" />
+      <FaceRecognition imageUrl = { this.state.imageUrl } />
+        
 
-    </div>
+    </div>)
     
-  );
+  };
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import './ImageLinkForm.css'
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className="f3 white">
@@ -10,8 +10,9 @@ const ImageLinkForm = () => {
             </p>
             <div>
                 <div className="center form shadow-5">
-                    <input className='f4 pa2 w-70 center' type='text'/>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer b--none'>Detect</button>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={ onInputChange }/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer b--none'
+                    onClick={ onButtonSubmit }>Detect</button>
                 </div>
             </div>
         </div>
